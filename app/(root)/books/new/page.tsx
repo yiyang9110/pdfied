c8be8394cd606adf8bc2e01486ca8dfee8c568c7
yiyang9110 <yiyang9110@gmail.com>
2026-04-25 01:19:41 +1000

    added login guard

--- app/(root)/books/new/page.tsx
+++ app/(root)/books/new/page.tsx
@@ -1,6 +1,8 @@
 import UploadForm from "@/components/UploadForm";
+import { auth } from "@clerk/nextjs/server";
 
-const Page = () => {
+const Page = async () => {
+  await auth.protect();
   return (
     <main className="wrapper container">
       <div className="mx-auto max-w-180 space-y-10">
